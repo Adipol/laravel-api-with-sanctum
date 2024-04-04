f6556ee3b8bb1070b2c24ad8bdf7a40b35d04029
work-pc <adipol13@gmail.com>
2024-04-04 17:18:26 -0400

    completo

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
+use GuzzleHttp\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,9 +22,20 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Route::get('posts', [PostController::class, 'index']);
-//Route::post('posts', [PostController::class, 'store']);
 
-Route::resource('posts', PostController::class);
 
+//Public router
+//Route::resource('posts', PostController::class);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::get('posts', [PostController::class, 'index']);
 Route::get('posts/search/{title}', [PostController::class, 'search']);
+
+//Private router
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('posts', [PostController::class, 'store']);
+    Route::get('posts/{id}', [PostController::class, 'show']);
+    Route::put('posts/{id}', [PostController::class, 'update']);
+    Route::delete('posts/{id}', [PostController::class, 'destroy']);
+    Route::post('logout', [AuthController::class, 'logout']);
+});
